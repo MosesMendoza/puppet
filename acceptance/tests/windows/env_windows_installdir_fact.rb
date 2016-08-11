f04279d2534a5a4c2544f580c29d313f3d720114
Moses Mendoza <mendoza.moses@gmail.com>
2016-08-11 16:13:30 -0700

    (maint) Add test for env_windows_installdir fact
    (PA-466) This commit adds a test to ensure the fact env_windows_installdir is
    present and set to the correct directory. This fact is set via the
    environment.bat file on Windows installations and recently was impacted by a
    regression in that file, so a test is warranted. This test is added as its own
    file as opposed to the 4654_facts_face.rb file because that test seemed in
    spirit to be testing the functionatlity of `puppet facts` rather than any
    specific fact, while this test is for an explicit fact.
    
    Signed-off-by: Moses Mendoza <mendoza.moses@gmail.com>

---
+++ acceptance/tests/windows/env_windows_installdir_fact.rb
@@ -0,0 +1,15 @@
+# (PA-466) The env_windows_installdir fact is set as an environment variable
+# fact via environment.bat on Windows systems. Test to ensure it is both
+# present and accurate.
+test_name 'PA-466: Ensure env_windows_installdir fact is present and correct' do
+
+  confine :to, :platform => 'windows'
+
+  agents.each do |agent|
+    step "test for presence/accurance of fact on #{agent}" do
+      on agent, puppet('facts') do
+        assert_match(/"env_windows_installdir": "C:\\\\Program Files\\\\Puppet Labs\\\\Puppet"/, stdout, "env_windows_installdir fact did not match expected output")
+      end
+    end
+  end
+end
