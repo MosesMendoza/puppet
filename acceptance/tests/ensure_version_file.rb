54feb25520338323cbd1e222bc0905dbd6942be8
Moses Mendoza <moses@puppet.com>
2016-08-11 17:30:08 -0700

    temp

--- acceptance/tests/ensure_version_file.rb
+++ acceptance/tests/ensure_version_file.rb
@@ -8,17 +8,17 @@ test_name 'PA-466: Ensure version file is created on agent' do
 
   skip_test 'requires version file which is created by AIO' if [:gem].include?(@options[:type])
 
-  step "test for existence of version file" do
-    agents.each do |agent|
-      version_file = agent[:platform] =~ /windows/ ?
-        'C:/Program Files/Puppet Labs/Puppet/VERSION' :
-        '/opt/puppetlabs/puppet/VERSION'
+  #step "test for existence of version file" do
+  #  agents.each do |agent|
+  #    version_file = agent[:platform] =~ /windows/ ?
+  #      'C:/Program Files/Puppet Labs/Puppet/VERSION' :
+  #      '/opt/puppetlabs/puppet/VERSION'
 
-      if !file_exists?(agent, version_file)
-        fail_test("Failed to find version file #{version_file} on agent #{agent}")
-      end
-    end
-  end
+  #    if !file_exists?(agent, version_file)
+  #      fail_test("Failed to find version file #{version_file} on agent #{agent}")
+  #    end
+  #  end
+  #end
   step "test for existence of a kmnown file" do
     agents.each do |agent|
       version_file = agent[:platform] =~ /windows/ ?
