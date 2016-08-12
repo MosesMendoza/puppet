321336730d324937c01ebd3f6de773a40d6f40ec
Moses Mendoza <mendoza.moses@gmail.com>
2016-08-11 17:14:01 -0700

    temp

--- acceptance/tests/windows/PA-466_env_windows_installdir_fact.rb
+++ acceptance/tests/windows/PA-466_env_windows_installdir_fact.rb
@@ -6,9 +6,15 @@ test_name 'PA-466: Ensure env_windows_installdir fact is present and correct' do
   confine :to, :platform => 'windows'
 
   agents.each do |agent|
-    step "test for presence/accurance of fact on #{agent}" do
+    step "test for presence/accurance of env_windows_installdir fact on #{agent}" do
       on agent, puppet('facts') do
         assert_match(/"env_windows_installdir": "C:\\\\Program Files\\\\Puppet Labs\\\\Puppet"/, stdout, "env_windows_installdir fact did not match expected output")
+      end
+    end
+
+    step "test for presence/accurance of domain fact on #{agent}" do
+      on agent, puppet('facts') do
+        assert_match(/"domain": "delivery.puppetlabs.net"/, stdout, "env_windows_installdir fact did not match expected output")
       end
     end
   end
