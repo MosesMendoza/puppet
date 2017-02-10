a8d65f447ca794e338b2111b25c9408ff89efe2a
Moses Mendoza <moses@puppet.com>
2017-02-10 15:30:01 -0800

    (PUP-7021) Add UTF-8-aware Etc wrapper
    The Ruby Etc module will return various Struct objects containing strings in
    the system's locale, or if they are invalid in the current locale, as
    ASCII-8BIT (Binary). This leads to inconsistent and problematic behavior when
    Puppet uses the Etc module in its providers, particularly when trying to
    concatenate or compare strings which now may have different, and often
    incompatible, encodings. The nameservice provider in particular makes heavy use
    of Etc, which has resulted in numerous bugs reported regarding UTF-8.
    
    Initial attempts to resolve the issues focused on the locations where string
    comparison or concatenation was happening, but it became quickly clear that
    there are too many places to do this, and the better solution would likely be
    to control the problem where the strings first enter the system - at the Etc
    module.
    
    This commit adds a Puppet wrapper around ruby's Etc module that leverages
    puppet/util/character_encoding to convert the values returned by Etc to UTF-8.
    In order to avoid ever letting the original Etc values get into the system (and
    with a side benefit of avoiding extraneous object creation) the Structs are
    modified in-place.
    
    It is true that this commit may introduce different failure scenarios, as
    unavoidable failure will now likely be located in the string conversion process
    (ie if we have truly UTF-8 incompatible strings), rather than higher in the
    stack at comparison or concatenation for display, but it seems these were going
    to fail anyway.
    
    Signed-off-by: Moses Mendoza <moses@puppet.com>

---
+++ spec/unit/etc_spec.rb
@@ -0,0 +1,155 @@
+require 'puppet/etc'
+require 'spec_helper'
+
+# The Ruby::Etc module is largely non-functional on Windows - many methods
+# simply return nil regardless of input, the Etc::Group struct is not defined,
+# and Etc::Passwd is missing fields
+describe Puppet::Etc, :if => !Puppet.features.microsoft_windows? do
+  let(:bin) { 'bin'.force_encoding(Encoding::ISO_8859_1) }
+  let(:root) { 'root'.force_encoding(Encoding::ISO_8859_1) }
+  let(:x) { 'x'.force_encoding(Encoding::ISO_8859_1) }
+  let(:daemon) { 'daemon'.force_encoding(Encoding::ISO_8859_1) }
+  let(:root_comment) { 'i am the root user'.force_encoding(Encoding::ISO_8859_1) }
+  let(:user_struct_iso_8859_1) { Etc::Passwd.new(root, x, 0, 0, root_comment) }
+  let(:group_struct_iso_8859_1) { Etc::Group.new(bin, x, 1, [root, bin, daemon]) }
+
+  # For the methods described which actually expect an encoding conversion, we
+  # only superficially test via #force_encoding - the deeper level testing is in
+  # character_encoding_spec.rb which handles testing transcoding etc.
+
+
+  describe "getgrent" do
+    context "given an original system Etc Group struct with ISO-8850-1 string values" do
+      before { Etc.expects(:getgrent).returns(group_struct_iso_8859_1) }
+      let(:converted) { Puppet::Etc.getgrent }
+
+      it "should return a struct with :name and :passwd field values converted to UTF-8" do
+        [converted.name, converted.passwd].each do |value|
+          expect(value.encoding).to eq(Encoding::UTF_8)
+        end
+      end
+
+      it "should return a struct with a :mem array with all field values converted to UTF-8" do
+        converted.mem.each { |elem| expect(elem.encoding).to eq(Encoding::UTF_8) }
+      end
+    end
+  end
+
+  describe "endgrent" do
+    it "should call Etc.getgrent" do
+      Etc.expects(:getgrent)
+      Puppet::Etc.getgrent
+    end
+  end
+
+  describe "setgrent" do
+    it "should call Etc.setgrent" do
+      Etc.expects(:setgrent)
+      Puppet::Etc.setgrent
+    end
+  end
+
+  describe "getpwent" do
+    before { Etc.expects(:getpwent).returns(user_struct_iso_8859_1) }
+    let(:converted) { Puppet::Etc.getpwent }
+    context "given an original system Etc Passwd struct with ISO-8859-1 string values" do
+      it "should return an Etc Passwd struct with field values converted to UTF-8" do
+        [converted.name, converted.passwd, converted.gecos].each do |value|
+          expect(value.encoding).to eq(Encoding::UTF_8)
+        end
+      end
+    end
+  end
+
+  describe "endpwent" do
+    it "should call Etc.endpwent" do
+      Etc.expects(:endpwent)
+      Puppet::Etc.endpwent
+    end
+  end
+
+  describe "setpwent" do
+    it "should call Etc.setpwent" do
+      Etc.expects(:setpwent)
+      Puppet::Etc.setpwent
+    end
+  end
+
+  describe "getpwnam" do
+    context "given a username to query" do
+      it "should call Etc.getpwnam with that username" do
+        Etc.expects(:getpwnam).with('foo')
+        Puppet::Etc.getpwnam('foo')
+      end
+    end
+
+    context "given an original system Etc Passwd struct with ISO-8859-1 string values" do
+      it "should return an Etc Passwd struct with field values converted to UTF-8" do
+        Etc.expects(:getpwnam).with('root').returns(user_struct_iso_8859_1)
+        converted = Puppet::Etc.getpwnam('root')
+        [converted.name, converted.passwd, converted.gecos].each do |value|
+          expect(value.encoding).to eq(Encoding::UTF_8)
+        end
+      end
+    end
+  end
+
+  describe "getgrnam" do
+    context "given a group name to query" do
+      it "should call Etc.getgrnam with that group name" do
+        Etc.expects(:getgrnam).with('foo')
+        Puppet::Etc.getgrnam('foo')
+      end
+    end
+
+    context "given an original system Etc Group struct with ISO-8859-1 string values" do
+      it "should return an Etc Group struct with field values converted to UTF-8" do
+        Etc.expects(:getgrnam).with('bin').returns(group_struct_iso_8859_1)
+        converted = Puppet::Etc.getgrnam('bin')
+        [converted.name, converted.passwd].each do |value|
+          expect(value.encoding).to eq(Encoding::UTF_8)
+        end
+        converted.mem.each { |elem| expect(elem.encoding).to eq(Encoding::UTF_8) }
+      end
+    end
+  end
+
+  describe "getgrgid" do
+    context "given a group ID to query" do
+      it "should call Etc.getgrgid with the id" do
+        Etc.expects(:getgrgid).with(0)
+        Puppet::Etc.getgrgid(0)
+      end
+    end
+
+    context "given an original Etc Group struct with field values in ISO-8859-1" do
+      it "should return an Etc Group struct with field values converted to UTF-8" do
+        Etc.expects(:getgrgid).with(1).returns(group_struct_iso_8859_1)
+        converted = Puppet::Etc.getgrgid(1)
+        [converted.name, converted.passwd].each do |value|
+          expect(value.encoding).to eq(Encoding::UTF_8)
+        end
+        converted.mem.each { |elem| expect(elem.encoding).to eq(Encoding::UTF_8) }
+      end
+    end
+  end
+
+  describe "getpwid" do
+    context "given a UID to query" do
+      it "should call Etc.getpwuid with the id" do
+        Etc.expects(:getpwuid).with(2)
+        Puppet::Etc.getpwuid(2)
+      end
+    end
+  end
+
+  context "given an orginal Etc Passwd struct with field values in ISO-8859-1" do
+    it "should return an Etc Passwd struct with field values converted to UTF-8" do
+      Etc.expects(:getpwuid).with(0).returns(user_struct_iso_8859_1)
+      converted = Puppet::Etc.getpwuid(0)
+      [converted.name, converted.passwd, converted.gecos].each do |value|
+        expect(value.encoding).to eq(Encoding::UTF_8)
+      end
+    end
+  end
+end
