89d547379dc1a45402fedb0476ad80d42ab6a637
Moses Mendoza <moses@puppet.com>
2017-10-04 17:09:23 -0700

    (PUP-2189) Test to prove broken concurrent cert revocation
    This commit adds a new integration spec test that validates that we can
    pseudo-concurrently revoke 100 certs and not leave behind a malformed CRL and
    actually revoke all the certs we want to. The test uses fork rather than
    threads to avoid thread-limits found on some systems. Within the forked process
    we generate a new ca object to avoid sharing copies of the test CA - we want to
    simulate many distinct ca invocations revoking certs. It currently fails,
    consistently only revoking about half of the expected certificates.
    
    Signed-off-by: Moses Mendoza <moses@puppet.com>

--- spec/integration/ssl/certificate_revocation_list_spec.rb
+++ spec/integration/ssl/certificate_revocation_list_spec.rb
@@ -3,15 +3,34 @@ require 'spec_helper'
 
 require 'puppet/ssl/certificate_revocation_list'
 
-describe Puppet::SSL::CertificateRevocationList do
-  include PuppetSpec::Files
+include PuppetSpec::Files
 
-  let(:dir) { tmpdir("ca_integration_testing") }
+describe Puppet::SSL::CertificateRevocationList, :if => !Puppet.features.microsoft_windows? do
 
-  before(:each) do
+  def generate_certificates!(concurrency_volume)
+    ca = Puppet::SSL::CertificateAuthority.new
+    concurrency_volume.times do |i|
+      ca.generate("host#{i}")
+    end
+  end
+
+  def perform_concurrent_revocations(concurrency_volume)
+    pids = []
+    concurrency_volume.times do |i|
+      pids << fork do
+        ca = Puppet::SSL::CertificateAuthority.new
+        ca.revoke("host#{i}")
+      end
+    end
+    pids.each { |pid| Process.wait(pid) }
+  end
+
+  let(:temp_dir) { tmpdir("ca_integration_testing") }
+
+  before do
     # Get a safe temporary file
-    Puppet.settings[:confdir] = dir
-    Puppet.settings[:vardir] = dir
+    Puppet.settings[:confdir] = temp_dir
+    Puppet.settings[:vardir] = temp_dir
     Puppet::SSL::Host.ca_location = :local
   end
 
@@ -21,10 +40,23 @@ describe Puppet::SSL::CertificateRevocationList do
     Puppet::SSL::Host.indirection.termini.clear
   end
 
+  let(:concurrency_volume) { 100 }
+
   it "should be able to read in written out CRLs with no revoked certificates" do
     ca = Puppet::SSL::CertificateAuthority.new
-    raise "CRL not created" unless Puppet::FileSystem.exist?(Puppet[:hostcrl])
-    crl = Puppet::SSL::CertificateRevocationList.new("crl_int_testing")
+    crl = Puppet::SSL::CertificateRevocationList.new("crl_in_testing")
     expect(crl.read(Puppet[:hostcrl])).to be_an_instance_of(OpenSSL::X509::CRL)
+  end
+
+  context "when handling concurrent access" do
+    it "should be able to support safe concurrent write access to the CRL" do
+      generate_certificates!(concurrency_volume)
+      perform_concurrent_revocations(concurrency_volume)
+      validator_ca = Puppet::SSL::CertificateAuthority.new
+      # is this even a well-formed CRL?
+      expect(validator_ca.crl.content).to be_an_instance_of(OpenSSL::X509::CRL)
+      # did we actually revoke all the certs we wanted to?
+      expect(validator_ca.crl.content.revoked.length).to eq(concurrency_volume)
+    end
   end
 end
